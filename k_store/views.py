b013ff0ca689038ae4625782a79268d44f9e8b31
Kenneth-ca <kenneth.ca95@gmail.com>
2019-12-05 01:55:16 +0000

    [feature] templates static

--- k_store/views.py
+++ k_store/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Hola mundo!')
+    return render(request, 'index.html', {
+        'message': 'Lista de productos',
+        'title': 'Productos',
+        'products': [
+            {'title': 'Playera', 'price': 5, 'stock': True},
+            {'title': 'Camisa', 'price': 7, 'stock': True},
+            {'title': 'Mochila', 'price': 20, 'stock': False},
+        ],
+    })
